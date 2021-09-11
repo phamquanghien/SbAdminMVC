addf11cdd15c9c6cd3e6352fd2ffdb585a27972b
Pham Quang Hien <phamquanghien@Phams-MacBook-Pro.local>
2021-09-11 15:07:36 +0700

    init

---
+++ Areas/Administrators/Controllers/HomeAdminController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyApp.Models;
+namespace MyApp.Areas.Administrators.Controllers{
+    public class HomeAdminController : Controller
+    {
+        private readonly ILogger<HomeAdminController> _logger;
+
+        public HomeAdminController(ILogger<HomeAdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
